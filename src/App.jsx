0417684051293ca09ecad663cb02a7a66b4f0ea5
jayteemoney <jethroirmiya@gmail.com>
2025-05-21 13:29:43 +0000

    added question component and app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 flex items-center justify-center">
+      
+    </div>
+  );
+};
+
+export default App;
